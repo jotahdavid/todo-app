69e5c65102d9a4c6732c8d89b6f6f0c39a914c71
Jhonatan David <82552187+jotahdavid@users.noreply.github.com>
2022-05-23 20:07:27 -0300

    feat: render todo list

--- src/scripts/app.js
+++ src/scripts/app.js
@@ -1,1 +1,25 @@
+import TodoItem from './components/TodoItem.js';
 
+const todoList = [
+  {
+    details: 'Do coding challenges',
+    completed: false
+  },
+  {
+    details: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Dignissimos alias soluta optio nulla officia corporis inventore esse expedita in rem saepe quis laborum ex similique unde, aliquam vero quae corrupti',
+    completed: true
+  }
+];
+
+function renderTodoList(todoList) {
+  const $todoList = document.querySelector('.todo-list');
+  const $fragment = document.createDocumentFragment();
+
+  $fragment.append(
+    ...todoList.map(todo => TodoItem(todo))
+  );
+
+  $todoList.appendChild($fragment);
+}
+
+renderTodoList(todoList);
